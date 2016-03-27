3feb419f408de1fe58d62b9a5e47f8cd0569cc8f
infernalslam <infernal-slam@hotmail.com>
2016-03-27 14:49:41 +0700

    test

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 /* global describe it assert */
+var num1 = 0
+var num2 = 0
 function sum (num1, num2) {
   return num1 + num2
 }
@@ -7,7 +9,7 @@ var assert = require('assert')
 describe('sum function', function () {
   describe('sum()', function () {
     it('should return 3 when the value pass', function () {
-      assert.equal(sum(1, 2), 3)
+      assert.equal(sum(num1, num2), 3)
     })
   })
 })
